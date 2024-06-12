aa4a86cb3a6ec371494f08a98025cd0c755261c2
Snehal Sakhare <snehal.sakhare@ibm.com>
2024-06-12 19:46:03 +0530

    Add files via upload

---
+++ SQL/Activities/Activity 11.sql
@@ -0,0 +1,49 @@
+REM   Script: Activity 11
+REM   FST Apr 2024
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders  
+WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order_date;
+
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+GROUP BY customer_id, order_date 
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
+create table customers ( 
+    customer_id int primary key, customer_name varchar(32), 
+    city varchar(20), grade int, salesman_id int);
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+INSERT ALL 
+    INTO customers VALUES (3002, 'Nick Rimando', 'New York', 100, 5001) 
+    INTO customers VALUES (3007, 'Brad Davis', 'New York', 200, 5001) 
+    INTO customers VALUES (3005, 'Graham Zusi', 'California', 200, 5002) 
+    INTO customers VALUES (3008, 'Julian Green', 'London', 300, 5002) 
+    INTO customers VALUES (3004, 'Fabian Johnson', 'Paris', 300, 5006) 
+    INTO customers VALUES (3009, 'Geoff Cameron', 'Berlin', 100, 5003) 
+    INTO customers VALUES (3003, 'Jozy Altidor', 'Moscow', 200, 5007) 
+    INTO customers VALUES (3001, 'Brad Guzan', 'London', 300, 5005) 
+SELECT 1 FROM DUAL;
+
+SELECT a.customer_name, a.city, a.grade, b.name AS "Salesman", b.city FROM customers a  
+LEFT OUTER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE a.grade<300  
+ORDER BY a.customer_id;
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
